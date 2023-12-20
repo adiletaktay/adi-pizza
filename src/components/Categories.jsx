8a73a8a6e83f45fbffbe8c7cc1bab85014898bee
ADI <adiletaktay@gmail.com>
2023-12-20 21:40:06 +0600

    fix category and sort

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,21 +1,17 @@
-import React, { useState } from "react";
+import React from "react";
 
-function Categories() {
-
-    const [activeCategories, setActiveCategories] = useState("Все");
-
+function Categories({ value, onChangeCategory}) {
     const categories = ["Все", "Мясные", "Вегетарианская", "Гриль", "Острые", "Закрытые"];
 
     return ( 
         <div className="categories">
             <ul>
-                {categories.map((item, i) => (
+                {categories.map((categoryName, i) => (
                     <li
                         key={i}
-                        onClick={() => setActiveCategories(item)}
-                        className={activeCategories === item ? "active" : ""}
-                    >
-                        {item}
+                        onClick={() => onChangeCategory(i)}
+                        className={value === i ? "active" : ""}>
+                        {categoryName}
                     </li>
                 ))}
             </ul>
