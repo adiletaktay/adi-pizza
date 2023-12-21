117f8a461877cfeb25fa6afed61450f267a5e09e
ADI <adiletaktay@gmail.com>
2023-12-21 21:36:22 +0600

    add search

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,7 @@ import Sort from '../components/Sort';
 import PizzaBlock from '../components/PizzaBlock';
 import Skeleton from '../components/PizzaBlock/Skeleton';
 
-const Home = () => {
+const Home = ({searchValue}) => {
     const [items, setItems] = React.useState([]);
     const [isLoading, setIsLoading] = React.useState(true);
     const [categoryId, setCategoryId] = React.useState(0);
@@ -24,6 +24,9 @@ const Home = () => {
         })
         window.scrollTo(0, 0);
     }, [categoryId, sortType]);
+    
+    const pizzas = items.map((obj) => <PizzaBlock key={obj.id} {...obj} />)
+    const skeletons = [...new Array(6)].map((_, index) => <Skeleton key={index} />)
 
     return (
         <div className='container'>
@@ -33,11 +36,7 @@ const Home = () => {
             </div>
             <h2 className="content__title">Все пиццы</h2>
             <div className="content__items">
-            {
-                isLoading 
-                ? [...new Array(6)].map((_, index) => <Skeleton key={index} />)
-                : items.map((obj) => <PizzaBlock key={obj.id} {...obj} />)
-            }
+            {isLoading ? skeletons: pizzas}
             </div>
         </div>
     )
