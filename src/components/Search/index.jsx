f219526a9b358b2f49f541ad05c40dd72827f4d6
ADI <adiletaktay@gmail.com>
2023-12-29 19:50:11 +0600

    add debounce and fix pagination

--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -1,15 +1,30 @@
 import React from 'react';
+import debounce from 'lodash.debounce';
 import { SearchContext } from '../../App';
 
 import styles from './Search.module.scss';
 
 export default function Search() {
-    const { searchValue, setSearchValue } = React.useContext(SearchContext);
+    const [value, setValue] = React.useState('');
+    const { setSearchValue } = React.useContext(SearchContext);
     const inputRef = React.useRef();
 
     const onClickClear = () => {
         setSearchValue('');
+        setValue('');
         inputRef.current.focus();
+    };
+    
+    const updateSearchValue = React.useCallback(
+        debounce((str) => {
+            setSearchValue(str);
+        }, 250),
+        [],
+    )
+
+    const onChangeInput = (event) => {
+        setValue(event.target.value);
+        updateSearchValue(event.target.value);
     };
 
   return (
@@ -25,11 +40,11 @@ export default function Search() {
         </svg>
         <input
             ref={inputRef}
-            value={searchValue}
-            onChange={(event) => setSearchValue(event.target.value)} 
+            value={value}
+            onChange={onChangeInput} 
             className={styles.input} 
             placeholder='Поиск пиццы ...' />
-        {searchValue && (
+        {value && (
             <svg 
             onClick={onClickClear}
                 className={styles.clearIcon}
